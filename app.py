5679e9ca0a8849d788b49d230dbc4aa4bd15260b
Nolan Taft <killer123yo@gmail.com>
2023-10-11 11:26:59 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import time
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+
+@app.route('/')
+def ShredHead():
+    return render_template('ShredHead.html')
+
+
+@app.route('/loadingScreen')
+def loadingScreen():
+    return render_template('loadingScreen.html')
+
+
+app.run(debug=True)
