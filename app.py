d14ac77a48702f1af01fe90cdd7040b491ba21dc
Nolan Taft <killer123yo@gmail.com>
2023-10-11 23:36:53 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
 import time
+from werkzeug.routing import BaseConverter
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
+
+# Define a custom URL converter to strip the .html extension
+
+
+class HTMLConverter(BaseConverter):
+    def to_python(self, value):
+        return value
+
+    def to_url(self, value):
+        return f"{value}.html"
+
+
+# Add the custom converter to the app
+app.url_map.converters['html'] = HTMLConverter
 
 
 @app.route('/')
@@ -14,3 +29,16 @@ def loadingScreen():
     return render_template('loadingScreen.html')
 
 
+@app.route('/play')
+def playScreen():
+    return render_template('playScreen.html')
+
+# Use the custom converter in your route
+
+
+@app.route('/<html:page>')
+def custom_pages(page):
+    return render_template(page)
+
+
+app.run(debug=True)
